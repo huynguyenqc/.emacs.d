bcc36cfcdcfe9e5fd1424c86ac6274c8622d3144
Phan Trinh <phanhonghanhtrinh@gmail.com>
2019-05-04 20:52:55 +0700

    Change theme

--- init.el
+++ init.el
@@ -33,14 +33,26 @@ package-archives
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (avy evil magit helm))))
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(blink-cursor-mode nil)
+ '(custom-enabled-themes (quote (solarized-dark)))
+ '(custom-safe-themes
+   (quote
+    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+ '(menu-bar-mode nil)
+ '(nyan-wavy-trail nil)
+ '(package-selected-packages (quote (nyan-mode solarized-theme avy evil magit helm)))
+ '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 128 :width normal :foundry "DAMA" :family "Ubuntu Mono")))))
 
 (use-package avy
   :ensure t
   :config (global-set-key (kbd "C-:") 'avy-goto-char))
+
+(global-set-key (kbd "C-x g") 'magit-status)
